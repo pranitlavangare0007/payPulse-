fb0500da5c241436f0d6f154b702927c2945a23a
Pranit Lavangare <pranitlavangare0007@gmail.com>
2026-04-05 16:43:14 +0530

    ready to deploy

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://paypulse-9rse.onrender.com",
   headers: { "Content-Type": "application/json" }
 });
 
